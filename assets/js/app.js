6d22084f57f954e6f48154d2d2f3021c2529fbe3
NadiaQN <quezada.nadia@gmail.com>
2017-11-07 10:44:44 -0300

    version 0.0.4 y 0.0.5

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 //mi funcion principal que creara los post y deshabilitara el boton
 var tweet = function(){
 //un if para inhabilitar el boton de enviar si no hay caracteres
-	if(document.getElementById('comment').value.length==0 || document.getElementById('comment').value.length>=140){
+	if(document.getElementById('comment').value.length==0 || document.getElementById('comment').value.length>=140 || document.getElementById('comment').value== " "){
 		document.getElementById('btn').setAttribute('disabled');
 	}else{
 //primero tomamos el comentario
@@ -23,21 +23,28 @@ var tweet = function(){
 //agregando los elementos al newtweet
 		newTweet.appendChild(paragraph);
 //agregando el div a section
-		wall.appendChild(newTweet);
+		wall.appendChild(newTweet);c
 	}
 };
 //agregando evento para el btn
  btn.addEventListener('click', tweet);
 //contador de caracteres
 function count(obj){
-var element = document.getElementById('display');
-element.innerHTML = 140 -obj.value.length;
+	var element = document.getElementById('display');
+	element.innerHTML = 140 -obj.value.length;
+	document.getElementById('comment').size = document.getElementById('comment').value.length;
 //un if para cambiar color segun caracteres
 	if(document.getElementById('comment').value.length >=120 ){
 		element.style.color = 'black';
 	}if(document.getElementById('comment').value.length >=130){
 		element.style.color = 'red';
 	}
-}
+};
+//funcion para auto resize textarea
+var textarea = document.getElementById("comment");
+var heightLimit = 200; /* Maximum height: 200px */
 
-
+textarea.oninput = function() {
+  textarea.style.height = ""; /* Reset the height*/
+  textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
+};
